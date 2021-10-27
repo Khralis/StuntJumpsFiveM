a2ae6ab05020c2e1cc3afe049fa1f850817195a1
Khralis <50895160+Khralis@users.noreply.github.com>
2021-10-26 23:57:45 -0400

    Correct the version number
    Did a dumb

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 name 'StuntJumpsFiveM'
 description 'Enables stunt jumps around the map'
 author 'Tom Grobbe'
-version '1.0?'
+version '1.0.1'
 
 files { 'Newtonsoft.Json.dll' }
 client_scripts { 'StuntJumpsFiveM.net.dll' }
