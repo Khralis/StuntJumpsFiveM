43be1eab028b0312e0fadbefc2a6e8c514a135f2
Khralis <50895160+Khralis@users.noreply.github.com>
2021-10-26 23:49:58 -0400

    Updated resource.lua to fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'adamant'
+game 'gta5'
+name 'StuntJumpsFiveM'
+description 'Enables stunt jumps around the map'
+author 'Tom Grobbe'
+version '1.0?'
+
+files { 'Newtonsoft.Json.dll' }
+client_scripts { 'StuntJumpsFiveM.net.dll' }
